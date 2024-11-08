d60c85bcc1f435ca34238496cbfe03c32c8b31a5
joe-tito2 <joetito1@gmail.com>
2024-11-08 17:21:30 -0500

    Initial commit

---
+++ delete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# List of environments to grep for in secret paths
+# Secret paths with these string will generate delete commands
+envs_to_delete=("DEV" "PROD")
+
+for path in `./vault-list-recursive.sh`; do
+    for env in ${envs_to_delete[@]}; do
+        if [[ $path =~ $env ]]; then
+            # echo $path | sed 's![^/]*$!!' | sed 's:/*$::'
+            path_to_delete=$(echo $path | sed 's![^/]*$!!' | sed 's:/*$::')
+            # path_to_delete=`$path | sed 's![^/]*$!!' | sed 's:/*$::'`
+            echo "./vault-vac.sh $path_to_delete 1"
+        fi
+    done
+done
